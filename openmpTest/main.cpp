bbfce9c7b79a0352149db39fb7ded8223fa9ce36
newsun-boki <1178303094@qq.com>
2021-07-17 12:48:15 +0800

    openmp

---
+++ openmpTest/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <omp.h>   // NEW ADD
+
+using namespace std;
+
+int main()
+{
+
+   
+	int sum = 0;
+	    #pragma omp parallel for num_threads(32)
+	    for(int i=0; i<100; i++)
+	    {
+		sum +=  i; 
+	    }
+
+	    cout << sum << endl;
+
+}
+
